1d5a3592af1a25c8c8c86381a7a6ea00e5823416
verraca <veronica.racca@gmail.com>
2018-05-22 21:22:17 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+const imagenes = ["./imgs/css3.jpg", "./imgs/github.png", "./imgs/html.png", "./imgs/javascript.png", "./imgs/node.jpg", "./imgs/react.png" ]
+var cartas = imagenes.concat(imagenes);
+
+var cartaDadaVuelta = "ninguna"
+const contraCara = "./imgs/ada.png" 
+var procesando= false
+
+var aciertos= 0
+
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+cartas= shuffle(cartas);
+
+$(".card").click(darVueltaCarta);
+
+function darVueltaCarta(){
+    var cardId = $(this).attr("id")
+    if(cartaDadaVuelta===cardId || procesando){
+        return
+    }
+    $(this).find("img").attr("src", cartas[cardId])
+
+    if(cartaDadaVuelta==="ninguna"){
+        cartaDadaVuelta= cardId
+
+    }else if (cartas[cartaDadaVuelta]===cartas[cardId]){
+        $("#" + cardId).fadeTo("fast", 0.5).off("click");
+        $("#" + cartaDadaVuelta).fadeTo("fast", 0.5).off("click");
+
+        cartaDadaVuelta="ninguna"
+        console.log("you choose wisley")
+        aciertos= aciertos+1
+        if(cartas.length/2===aciertos){
+            setTimeout(function(){
+            alert("WE ARE THE CHAMPIONS, MY FRIEND")
+        },500)
+        }
+
+
+    }else{ 
+        procesando=true;
+        setTimeout(function(){
+            console.log("you choose poorly")
+            $("#" + cardId).find("img").attr("src",contraCara)
+            $("#" + cartaDadaVuelta).find("img").attr("src",contraCara)
+            cartaDadaVuelta="ninguna"
+            procesando= false
+        },500)
+     
+    }  
+
+};
+
+
+
+
+
+
+
+
+
